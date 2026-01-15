ad37cc061fac9414ce14c7f806fc234f99988693
23f3003663 <23f3003663@ds.study.iitm.ac.in>
2026-01-15 22:10:09 +0530

    Explicitly pass datasourceUrl in Prisma Client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const adapter = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL || "file:./dev.db"
+})
+
+export const prisma = globalForPrisma.prisma || adapter
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
