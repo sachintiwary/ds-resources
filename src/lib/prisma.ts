02fe2ca6a18299c365435b8fac9e40ce0f05357e
23f3003663 <23f3003663@ds.study.iitm.ac.in>
2026-01-15 22:38:44 +0530

    Downgrade to Prisma 6 to fix deployment issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,13 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-    // @ts-ignore
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL || "file:./dev.db"
-        }
-    }
-})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
